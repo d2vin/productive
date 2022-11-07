e4fc1d21b4835c433865c47a08891bf5677f74a6
Devin Minnihan <devinminnihan@gmail.com>
2022-11-07 15:32:58 -0500

    add &apos;

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -33,11 +33,11 @@ const Profile: React.FC = () => {
                 404
               </h1>
               <p className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
-                Something's missing.
+                Something&apos;s missing.
               </p>
               <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-                Sorry, we can't find that page. You'll find lots to explore on
-                the home page.{" "}
+                Sorry, we can&apos;t find that page. You&apos;ll find lots to
+                explore on the home page.{" "}
               </p>
               <Link href="/home">
                 <a className="bg-primary-600 hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-900 my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium text-black focus:outline-none focus:ring-4">
