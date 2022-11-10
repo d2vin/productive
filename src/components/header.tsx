1387a1fd4ee36ce9d4edd4e76eb3c45803be9d78
Devin Minnihan <devinminnihan@gmail.com>
2022-11-10 11:18:13 -0500

    remove search bars

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -48,7 +48,7 @@ const Header: React.FC<HeaderProps> = ({ message }) => {
 
   return (
     <div className="fixed top-0 z-20 w-full border-b bg-white shadow-sm">
-      <div className="mx-5 flex max-w-5xl items-center justify-between bg-white lg:mx-auto">
+      <div className="mx-5 flex max-w-5xl items-center justify-between bg-white lg:mx-auto h-16">
         {/* Left - Logo */}
         <div className="relative hidden cursor-pointer lg:inline-grid ">
           <h1
@@ -64,20 +64,10 @@ const Header: React.FC<HeaderProps> = ({ message }) => {
         >
           <Image src="/productive.png" alt="Logo" height={40} width={40} />
         </div>
-        <form onSubmit={onSubmit}>
           {/* Middle - Search Input */}
+        {/* <form onSubmit={onSubmit}>
           <div className="max-w-xs">
             <div className="relative rounded-md p-3">
-              {/* <div className="pointer-events-none absolute inset-y-0 flex items-center pl-3">
-                <SearchIcon className="h-5 w-5 text-gray-500" />
-              </div>
-              <input
-                type="text"
-                placeholder="address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="block w-full rounded-md border-gray-300 bg-gray-50 pl-10 focus:border-black focus:ring-black sm:text-sm"
-              /> */}
               <div className="w-full">
                 <Combobox value={address} onChange={handleSelect}>
                   <div className="relative">
@@ -153,7 +143,7 @@ const Header: React.FC<HeaderProps> = ({ message }) => {
               </div>
             </div>
           </div>
-        </form>
+        </form> */}
         {/* Right */}
         <div className="flex items-center justify-end space-x-4">
           <MenuIcon
