b849cdacd9602c705a9bfacea8f9e56e1a91d254
Devin Minnihan <devinminnihan@gmail.com>
2022-11-22 09:51:32 -0500

    map zoom fix

--- src/components/places-autocomplete.tsx
+++ src/components/places-autocomplete.tsx
@@ -60,8 +60,9 @@ const PlacesAutocomplete: React.FC<PlacesAutocompleteProps> = ({
           <div className="relative mt-1">
             <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
               <Combobox.Input
+                type="text"
                 ref={inputRef}
-                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                className="w-full border-none py-2 pl-3 pr-10 text-base leading-5 text-gray-900 focus:ring-0"
                 placeholder="address"
                 onChange={(event) => setValue(event.target.value)}
                 value={value}
